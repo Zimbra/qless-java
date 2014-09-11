40f64cb8165f56f73e1b95b7d00009dcb02ada99
John Zhu <john.zhu@moz.com>
2014-09-11 14:57:09 -0700

    add qless exception wrapper class

---
+++ qless-java/src/main/java/com/zimbra/qless/QlessException.java
@@ -0,0 +1,19 @@
+package com.zimbra.qless;
+
+@SuppressWarnings("serial")
+public class QlessException extends RuntimeException {
+  public QlessException() {
+  }
+
+  public QlessException(String message) {
+    super(message);
+  }
+
+  public QlessException(Throwable cause) {
+    super(cause);
+  }
+
+  public QlessException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
